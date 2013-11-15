22534083594071f54a4ab8128231486cbbcea0c5
Fozefy <fozefy@gmail.com>
2013-11-15 04:22:28 -0500

    Fixed the testing functions
    All of the uncommented testing functions now run without error as they
    fit in with the new GA. Most of them have been checked and are working
    correctly, the ones I'm not sure about have been noted as comments.

--- Fixed Version/GAtesting.R
+++ Fixed Version/GAtesting.R
@@ -67,11 +67,6 @@ size(pop1)
 size(pop3)
 
 ### testing mutation
-
-# The mutation controller was removed, if we want to continue to have test cases which don't have to create the full GA we'll need to create test functions
-# mcontroller1 <- new("mutation.controller", 0.2, alleles.binary)
-# mcontroller2 <- new("mutation.controller", 1.0, alleles.binary)
-
 mutationEnv1<- new.GA.env(pop.size = 10, 
                    encoding.args = new.encoding.args(chr.length = 8,chr.encode.type = "binary"),
                    mutation.args = new.mutation.args(mutation.type = "binary", prob.mutation = 0.2))
@@ -100,7 +95,7 @@ ga.print(mresults1[[1]])
 ga.env4 <- new.GA.env(pop.size = 10, 
                       encoding.args = new.encoding.args(chr.length = 8,chr.encode.type = "symbolic",gene.alphabet = c("a", "b", "c")),
                       mutation.args = new.mutation.args(mutation.type = "symbolic", prob.mutation = 0.3))
-mutate <- mutate.fn(ga.env4$reproduction.env)
+mutate <- mutate.fgen(ga.env4$reproduction.env)
 pop4a <- new.population(ga.env4)
 pop4b <- duplicate(pop4a)
 mr <- mutate(pop4b[1:5])
@@ -122,39 +117,39 @@ with(ga.env4, {
 
 ######WORKING UP TO HERE
 
-chr2.vector <- duplicate(pop2)
-mutate2.fn <- mutation.genes.fgen(mutation.genes.creep, max=10, min=0)
-mutate2 <- mutation.fgen(prob.mutation = 0.2, mutation.genes.fn = mutate2.fn)
-mresults2 <- mutate2(chr2.vector)
-print.population(pop2, " ")
-print.chromosomes(chr2.vector, " ")
-print.returnValues.mutation(mresults2)
-
-chr3.vector <- duplicate.chromosomes(pop2)
-mutate3.fn <- mutation.genes.fgen(mutation.genes.integer, 0:9)
-mutate3 <- mutation.fgen(prob.mutation = 0.8, mutation.genes.fn = mutate3.fn)
-mresults3 <- mutate3(chr3.vector)
-print.population(pop2, " ")
-print.chromosomes(chr3.vector, " ")
-print.returnValues.mutation(mresults3)
-
-new.chr.spin <- new.chromosome.fgen(new.chromosome.spin)
-pop4 <- new.random.population(10, 5, new.chr.spin)
-evalutate.population(pop4, one.max.fn, one.max.decode)
-print.population(pop4, " ", "<>")
-chr4.vector <- duplicate.chromosomes(pop4)
-mutate4 <- mutation.fgen(prob.mutation = 0.2, mutation.genes.fn = mutation.genes.spin)
-mresults4 <- mutate4(chr4.vector)
-print.population(pop4, " ")
-print.chromosomes(chr4.vector, " ")
-print.returnValues.mutation(mresults4)
+# chr2.vector <- duplicate(pop2)
+# mutate2.fn <- mutation.genes.fgen(mutation.genes.creep, max=10, min=0)
+# mutate2 <- mutation.fgen(prob.mutation = 0.2, mutation.genes.fn = mutate2.fn)
+# mresults2 <- mutate2(chr2.vector)
+# print.population(pop2, " ")
+# print.chromosomes(chr2.vector, " ")
+# print.returnValues.mutation(mresults2)
+# 
+# chr3.vector <- duplicate.chromosomes(pop2)
+# mutate3.fn <- mutation.genes.fgen(mutation.genes.integer, 0:9)
+# mutate3 <- mutation.fgen(prob.mutation = 0.8, mutation.genes.fn = mutate3.fn)
+# mresults3 <- mutate3(chr3.vector)
+# print.population(pop2, " ")
+# print.chromosomes(chr3.vector, " ")
+# print.returnValues.mutation(mresults3)
+# 
+# new.chr.spin <- new.chromosome.fgen(new.chromosome.spin)
+# pop4 <- new.random.population(10, 5, new.chr.spin)
+# evalutate.population(pop4, one.max.fn, one.max.decode)
+# print.population(pop4, " ", "<>")
+# chr4.vector <- duplicate.chromosomes(pop4)
+# mutate4 <- mutation.fgen(prob.mutation = 0.2, mutation.genes.fn = mutation.genes.spin)
+# mresults4 <- mutate4(chr4.vector)
+# print.population(pop4, " ")
+# print.chromosomes(chr4.vector, " ")
+# print.returnValues.mutation(mresults4)
 
 ### testing crossover
-ga.env5a <- new.GA.env(chr.length = 8, pop.size = 10, chr.encode.type = "symbolic", 
-								chr.encode.args = chr.encode.args(gene.alphabet = c("a", "b", "c")),
-								mutation.type = "symbolic", prob.mutation = 0.3,
-								xover.type = "one.pt", xover.prob = 1.0)
-xover <- xover.fn(ga.env5a)
+ga.env5a <- new.GA.env(pop.size = 10, 
+								encoding.args = new.encoding.args(chr.length = 8, gene.alphabet = c("a", "b", "c"), chr.encode.type = "symbolic"),
+                mutation.args = new.mutation.args(mutation.type = "symbolic", prob.mutation = 0.3),
+                xover.args = new.xover.args(xover.type = "one.pt", xover.prob = 1.0))
+xover <- xover.fgen(ga.env5a$reproduction.env$xover.swapMask)
 pop5a.org <- new.population(ga.env5a)
 pop5a.dup <- duplicate(pop5a.org)
 p5a.dup.v1 <- pop5a.dup[(1:10)%%2==0]
@@ -164,11 +159,11 @@ ga.print(pop5a.org, gene.sep = "")
 ga.print(pop5a.dup, gene.sep = "")
 ga.print(xresults1, print.xpts = TRUE)
 
-ga.env5b <- new.GA.env(chr.length = 8, pop.size = 10, chr.encode.type = "symbolic", 
-								chr.encode.args = chr.encode.args(gene.alphabet = c("a", "b", "c")),
-								mutation.type = "symbolic", prob.mutation = 0.3,
-								xover.type = "k.pt", xover.args = new.xover.args(xover.k = 2), xover.prob = 1.0)
-xover <- xover.fn(ga.env5b)
+ga.env5b <- new.GA.env(pop.size = 10, 
+                encoding.args = new.encoding.args(gene.alphabet = c("a", "b", "c"),chr.length = 8, chr.encode.type = "symbolic"),
+								mutation.args = new.mutation.args(mutation.type = "symbolic", prob.mutation = 0.3),
+								xover.args = new.xover.args(xover.type = "k.pt",xover.k = 2,xover.prob = 1.0))
+xover <- xover.fgen(ga.env5b$reproduction.env$xover.swapMask)
 pop5b.org <- new.population(ga.env5b)
 pop5b.dup <- duplicate(pop5b.org)
 p5b.dup.v1 <- pop5b.dup[(1:10)%%2==0]
@@ -178,51 +173,52 @@ ga.print(pop5b.org, gene.sep = "")
 ga.print(pop5b.dup, gene.sep = "")
 ga.print(xresults2, print.xpts = TRUE)
 
-chr2.vector <- duplicate.chromosomes(pop2)
+chr2.vector <- duplicate(pop2)
 p1.vector <- chr2.vector[(1:10)%%2==0]
 p2.vector <- chr2.vector[(1:10)%%2==1]
-print.chromosomes(chr2.vector, " ")
-unif.xover <- xover.fgen(xover.mask.uniform, 0.3)
+ga.print(chr2.vector)
+unif.xover <- xover.fgen(xover.swapMask.fgen(xover.mask.uniform, 0.3))
 xresults2 <- unif.xover(p1.vector, p2.vector)
-print.chromosomes(chr2.vector, " ")
-print.returnValues.xover(xresults2)
+ga.print(chr2.vector)
+ga.print(xresults2)
 
-chr1.vector <- duplicate.chromosomes(pop1)
+chr1.vector <- duplicate(pop1)
 p1.vector <- chr1.vector[(1:10)%%2==0]
 p2.vector <- chr1.vector[(1:10)%%2==1]
-print.chromosomes(chr1.vector, " ")
-threePt.xover <- xover.fgen(xover.mask.kpoint, 3, TRUE)
+ga.print(chr1.vector)
+threePt.xover <- xover.fgen(xover.swapMask.fgen(xover.mask.kpoint, 3, TRUE))
 xresults2 <- threePt.xover(p1.vector, p2.vector)
-print.chromosomes(chr1.vector, " ")
-print.returnValues.xover(xresults2, print.xpts = TRUE)
+ga.print(chr1.vector)
+ga.print(xresults2, print.xpts = TRUE)
 
 ### Selection
-
-pop3 <- new.random.population(20, 32)
-evalutate.population(pop3, one.max.fn, one.max.decode)
-pop4 <- new.random.population(30, 32)
-evalutate.population(pop4, one.max.fn, one.max.decode)
-print.population(pop3, "")
-tournament.selection(8, tourn.size = 2, pop = pop3, decreasing = TRUE, verbose = TRUE)
-tournament.selection(8, tourn.size = 2, pop = pop3, prob.select.worse = 0.2,
-							decreasing = TRUE, verbose = TRUE)
-tournament.selection(8, tourn.size = 2, pop = pop3, prob.select.worse = 0.2, decreasing = TRUE)
-simple.tournament.selection(8, tourn.size = 2, pop = pop3, decreasing = TRUE, verbose = TRUE)
-simple.tournament.selection(8, tourn.size = 2, pop = pop3, decreasing = TRUE)
-st.selection <- select.population.fgen(tournament.selection, tourn.size = 2, 
-												      prob.select.worse = 0.2, decreasing = TRUE)
+#Tournament selection runs, not sure how to know if its 'correct'
+ga.env6a <- new.GA.env(pop.size = 20)
+pop3 <- new.population(ga.env6a)
+evaluate(pop3, one.max.fn, one.max.decode)
+ga.env6b <- new.GA.env(pop.size = 30)
+pop4 <- new.population(ga.env6b)
+evaluate(pop4, one.max.fn, one.max.decode)
+ga.print(pop3)
+tournament.selection(selection.size=8, tourn.size = 2, pop = pop3, decreasing = TRUE, verbose = TRUE)
+tournament.selection(selection.size=8, tourn.size = 2, pop = pop3, prob.select.worse = 0.2, decreasing = TRUE, verbose = TRUE)
+tournament.selection(selection.size=8, tourn.size = 2, pop = pop3, prob.select.worse = 0.2, decreasing = TRUE)
+simple.tournament.selection(selection.size=8, tourn.size = 2, pop = pop3, decreasing = TRUE, verbose = TRUE)
+simple.tournament.selection(selection.size=8, tourn.size = 2, pop = pop3, decreasing = TRUE)
+st.selection <- select.population.fgen(tournament.selection, tourn.size = 2, prob.select.worse = 0.2, decreasing = TRUE)
 st.selection(8, pop3)
-st.selection(50, pop4)
+st.selection(25, pop4)
 
 
+#Not sure what this section is doing, the run but do they work correctly?
 fitnessProportional.setup(pop3)
 cumulative.fitness(pop3)
 fitnessProportional.selection(8, pop3, verbose = TRUE)
 fitnessProportional.selection(8, pop3)
 
-print.population(pop4, "")
+ga.print(pop4)
 elite.selection(pop4, 3)
-print.population(pop3, "")
+ga.print(pop3)
 elite.selection(pop3, 3)
 top4 <- select.elite.population.fgen(elite.selection, elite.size=4)
 top4(pop4)
