c87298d00f5d384da491be602e58a48a3992b9de
Fozefy <fozefy@gmail.com>
2013-11-15 02:32:13 -0500

    Removed unneeded file

--- Fixed Version/GenticAlgo/other_possiblyUnused.R
+++
@@ -1,8 +0,0 @@
-###################Adds ability to merge two environments
-extend.env <- function(env, var.list){
-  for(name in names(var.list)){
-    env[[name]] <- var.list[[name]]
-  }
-  env
-}
-
