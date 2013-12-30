6340a8cc446c2107ec80edec628a6c392274fce3
Fozefy <fozefy@gmail.com>
2013-12-30 02:44:13 -0500

    Coevolutonary spatial tester added
    Added a testing function for coevo-spatial.
    
    Also created a function to generate a basic grid (torid) structure for
    any 'perfect square' number.

--- Testing Functions/spatialConstructors.R
+++ Testing Functions/spatialConstructors.R
@@ -25,7 +25,7 @@ testfun<-function(value, index)
   }
 }
 test = 1:100
-graph = mapply(testfun,test, seq_along(test))
+graph = t(mapply(testfun,test, seq_along(test)))
 
 #Complete Graph
 testfun<-function(value)
@@ -35,5 +35,60 @@ testfun<-function(value)
 test = 1:100
 graph = mapply(testfun,test)
 
+#Creates a grid structure - popSize must be a perfect square
+gridConstructor <- function(popSize)
+{
+  sideLength = sqrt(popSize)
+  graph = matrix(nrow=popSize,ncol=4)
+  for (i in 1:popSize)
+  {
+    #Connection above
+    if(i <= sideLength)
+    {
+      graph[i,1] = popSize - sideLength + i
+    }
+    else
+    {
+      graph[i,1] = i - sideLength
+    }
+    
+    #Connection below
+    if(i > popSize - sideLength)
+    {
+      graph[i,2] = sideLength + i - popSize
+    }
+    else
+    {
+      graph[i,2] = i + sideLength
+    }
+    
+    #Connection to the left
+    if (i %% sideLength == 1)
+    {
+      graph[i,3] = i + sideLength - 1
+    }
+    else
+    {
+      graph[i,3] = i - 1
+    }
+    
+    #Connection to the right
+    if (i %% sideLength == 0)
+    {
+      graph[i,4] = i - sideLength + 1
+    }
+    else
+    {
+      graph[i,4] = i + 1
+    }
+  }
+  
+  graph
+}
+graph = gridConstructor(100)
+
 #Test the spatial constructor
 ga = new.GA.env(selection.args=new.selection.args(adjMatrix=graph))
+
+#Spatial Coevolutionary
+ga = new.GA.env(GA.base.args=new.GA.base.args(numPop=2), fitness.args=new.fitness.args(fitness.fn=twoPop.one.max, goal=90, externalConnectionsMatrix=matrix(c(1:100, 1:100), nrow=100, ncol=2)), xover.args = new.xover.args(keepSecondaryParent=FALSE), selection.args=new.selection.args(elitism=TRUE,adjMatrix=graph))
