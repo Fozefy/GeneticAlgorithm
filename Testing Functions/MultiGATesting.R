19df04c3c957bb3e160f3819072b0c448c689bc6
Fozefy <fozefy@gmail.com>
2014-01-13 04:55:48 -0500

    Adding new spatial functions
    Created ring functions to test with. Also created a experimental data
    file holding basic data from experiments run

--- Testing Functions/MultiGATesting.R
+++ Testing Functions/MultiGATesting.R
@@ -1,9 +1,10 @@
 source("Testing Functions/spatialConstructors.R")
 
 #Test Many
-graph = gridConstructor(100)
 n=40
+graph = gridConstructor(100)
 
+#With Elite
 results = vector("list",n)
 generations = c(1)
 maxFit = c(1)
@@ -18,6 +19,7 @@ for (i in 1:n)
   rm(ga)
 }
 
+#No ELite
 results2 = vector("list",n)
 generations2 = c(1)
 maxFit2 = c(1)
@@ -29,20 +31,6 @@ for (i in 1:20)
   generations2[i] = ga$gen
   maxFit2[i] = ga$currentGen.results@maxFit@fitness$value
   print(paste(i,"Complete"))
-}
-
-#Print fitness result graphs to files
-for (a in 1:n)
-{
-  fitNum = a
-  fitness = NULL
-  for (i in 2:length(results2[[fitNum]]))
-  {
-    fitness[[i-1]] = results2[[fitNum]][[i]]@currentGen.results@maxFit@fitness$value
-  }
-  jpeg(paste("plot",a,"_NoElite.jpg"))
-  plot(fitness)
-  dev.off()
 }
 
 results3 = vector("list",n)
@@ -73,8 +61,8 @@ for (i in 1:20)
 
 #Testing Spatial Effects
 n=20
-graph = gridConstructor(100) #4 connections
 
+graph = gridConstructor(100) #4 connections
 results = vector("list",n)
 generations = c(1)
 maxFit = c(1)
@@ -118,4 +106,96 @@ for (i in 1:n)
   maxFit3[i] = ga$currentGen.results@maxFit@fitness$value
   print(paste(i,"Complete"))
   rm(ga)
+}
+
+graph = ring.graph(100) #ring graph
+results4 = vector("list",n)
+generations4 = c(1)
+maxFit4 = c(1)
+for (i in 1:n)
+{
+  ga = new.GA.env(GA.base.args=new.GA.base.args(max.gen=500,numPop=2), fitness.args=new.fitness.args(fitness.fn=twoPop.one.max, goal=90, externalConnectionsMatrix=matrix(c(1:100, 1:100), nrow=100, ncol=2)), xover.args = new.xover.args(keepSecondaryParent=FALSE), selection.args=new.selection.args(elitism=TRUE,adjMatrix=graph), verbose=FALSE)
+  generational.ga(ga)
+  results4[[i]]=ga$reported.data
+  generations4[i] = ga$gen
+  maxFit4[i] = ga$currentGen.results@maxFit@fitness$value
+  print(paste(i,"Complete"))
+  rm(ga)
+}
+
+graph = ring.graph.extra(100) #ring graph more connection
+results5 = vector("list",n)
+generations5 = c(1)
+maxFit5 = c(1)
+for (i in 1:n)
+{
+  ga = new.GA.env(GA.base.args=new.GA.base.args(max.gen=500,numPop=2), fitness.args=new.fitness.args(fitness.fn=twoPop.one.max, goal=90, externalConnectionsMatrix=matrix(c(1:100, 1:100), nrow=100, ncol=2)), xover.args = new.xover.args(keepSecondaryParent=FALSE), selection.args=new.selection.args(elitism=TRUE,adjMatrix=graph), verbose=FALSE)
+  generational.ga(ga)
+  results5[[i]]=ga$reported.data
+  generations5[i] = ga$gen
+  maxFit5[i] = ga$currentGen.results@maxFit@fitness$value
+  print(paste(i,"Complete"))
+  rm(ga)
+}
+
+#ring graph w/ small world
+results6 = vector("list",n)
+generations6 = c(1)
+maxFit6 = c(1)
+for (i in 1:n)
+{
+  graph = ring.graph.smallWorld(100)
+  ga = new.GA.env(GA.base.args=new.GA.base.args(max.gen=500,numPop=2), fitness.args=new.fitness.args(fitness.fn=twoPop.one.max, goal=90, externalConnectionsMatrix=matrix(c(1:100, 1:100), nrow=100, ncol=2)), xover.args = new.xover.args(keepSecondaryParent=FALSE), selection.args=new.selection.args(elitism=TRUE,adjMatrix=graph), verbose=FALSE)
+  generational.ga(ga)
+  results6[[i]]=ga$reported.data
+  generations6[i] = ga$gen
+  maxFit6[i] = ga$currentGen.results@maxFit@fitness$value
+  print(paste(i,"Complete"))
+  rm(ga)
+}
+
+#random graph - 4 connections
+results7 = vector("list",n)
+generations7 = c(1)
+maxFit7 = c(1)
+for (i in 1:n)
+{
+  graph =randomConstructor(4,100)
+  ga = new.GA.env(GA.base.args=new.GA.base.args(max.gen=500,numPop=2), fitness.args=new.fitness.args(fitness.fn=twoPop.one.max, goal=90, externalConnectionsMatrix=matrix(c(1:100, 1:100), nrow=100, ncol=2)), xover.args = new.xover.args(keepSecondaryParent=FALSE), selection.args=new.selection.args(elitism=TRUE,adjMatrix=graph), verbose=FALSE)
+  generational.ga(ga)
+  results7[[i]]=ga$reported.data
+  generations7[i] = ga$gen
+  maxFit7[i] = ga$currentGen.results@maxFit@fitness$value
+  print(paste(i,"Complete"))
+  rm(ga)
+}
+
+#random graph - 8 connections
+results8 = vector("list",n)
+generations8 = c(1)
+maxFit8 = c(1)
+for (i in 1:n)
+{
+  graph =randomConstructor(8,100)
+  ga = new.GA.env(GA.base.args=new.GA.base.args(max.gen=500,numPop=2), fitness.args=new.fitness.args(fitness.fn=twoPop.one.max, goal=90, externalConnectionsMatrix=matrix(c(1:100, 1:100), nrow=100, ncol=2)), xover.args = new.xover.args(keepSecondaryParent=FALSE), selection.args=new.selection.args(elitism=TRUE,adjMatrix=graph), verbose=FALSE)
+  generational.ga(ga)
+  results8[[i]]=ga$reported.data
+  generations8[i] = ga$gen
+  maxFit8[i] = ga$currentGen.results@maxFit@fitness$value
+  print(paste(i,"Complete"))
+  rm(ga)
+}
+
+#Print fitness result graphs to files
+for (a in 1:n)
+{
+  fitNum = a
+  fitness = NULL
+  for (i in 2:length(results2[[fitNum]]))
+  {
+    fitness[[i-1]] = results2[[fitNum]][[i]]@currentGen.results@maxFit@fitness$value
+  }
+  jpeg(paste("plot",a,"_NoElite.jpg"))
+  plot(fitness)
+  dev.off()
 }
--- Testing Functions/spatialConstructors.R
+++ Testing Functions/spatialConstructors.R
@@ -27,6 +27,180 @@ testfun<-function(value, index)
 #test = 1:100
 #graph = t(mapply(testfun,test, seq_along(test)))
 
+ring.graph <- function(popSize)
+{
+  graph = matrix(nrow=popSize,ncol=4)
+  for (i in 1:popSize)
+  {
+    #Less
+    if (i == 1)
+    {
+      graph[i,1] = popSize
+      graph[i,2] = popSize - 1
+    }
+    else if (i == 2)
+    {
+      graph[i,1] = popSize
+      graph[i,2] = 1
+    }
+    else
+    {
+      graph[i,1] = i - 1
+      graph[i,2] = i - 2
+    }
+    
+    #More
+    if (i == popSize)
+    {
+      graph[i,3] = 1
+      graph[i,4] = 2
+    }
+    else if (i == popSize - 1)
+    {
+      graph[i,3] = popSize
+      graph[i,4] = 1
+    }
+    else
+    {
+      graph[i,3] = i + 1
+      graph[i,4] = i + 2
+    }
+  }
+  
+  graph
+}
+
+ring.graph.extra <- function(popSize)
+{
+  graph = matrix(nrow=popSize,ncol=8)
+  for (i in 1:popSize)
+  {
+    #Less
+    if (i == 1)
+    {
+      graph[i,1] = popSize
+      graph[i,2] = popSize-1
+      graph[i,3] = popSize-2
+      graph[i,4] = popSize-3
+    }
+    else if (i == 2)
+    {
+      graph[i,1] = i - 1
+      graph[i,2] = popSize
+      graph[i,3] = popSize-1
+      graph[i,4] = popSize-2
+    }
+    else if (i == 3)
+    {
+      graph[i,1] = i - 1
+      graph[i,2] = i - 2
+      graph[i,3] = popSize
+      graph[i,4] = popSize-1
+    }
+    else if (i == 4)
+    {
+      graph[i,1] = i - 1
+      graph[i,2] = i - 2
+      graph[i,3] = i - 3
+      graph[i,4] = popSize
+    }
+    else
+    {
+      graph[i,1] = i - 1
+      graph[i,2] = i - 2
+      graph[i,3] = i - 3
+      graph[i,4] = i - 4
+    }
+    
+    #More
+    if (i == popSize)
+    {
+      graph[i,5] = 1
+      graph[i,6] = 2
+      graph[i,7] = 3
+      graph[i,8] = 4
+    }
+    else if (i == popSize - 1)
+    {
+      graph[i,5] = popSize
+      graph[i,6] = 1
+      graph[i,7] = 2
+      graph[i,8] = 3
+    }
+    else if (i == popSize - 2)
+    {
+      graph[i,5] = i+1
+      graph[i,6] = i+2
+      graph[i,7] = 1
+      graph[i,8] = 2
+    }
+    else if (i == popSize - 3)
+    {
+      graph[i,5] = i+1
+      graph[i,6] = i+2
+      graph[i,7] = i+3
+      graph[i,8] = 1
+    }
+    else
+    {
+      graph[i,5] = i + 1
+      graph[i,6] = i + 2
+      graph[i,7] = i + 3
+      graph[i,8] = i + 4
+    }
+  }
+  
+  graph
+}
+
+#Create a ring with some nodes connected at random to any other nodes
+#Watts Strogatx model
+ring.graph.smallWorld <- function(popSize)
+{
+  graph = ring.graph(popSize)
+  graph = cbind(graph,0)
+  
+  for (i in 1:popSize)
+  {
+    #1 in 5 nodes will have an extra connecion (making it 2 in 5 as we need a reciever as well)
+    if (sample(1:5,1) == 1)
+    {
+      connector = sample(1:(popSize-1),1)
+      if (connector == i) connector = popSize #We don't want a self connection
+      for(j in 5:length(graph[connector,]))
+      {
+        if (graph[connector,j] == 0)
+        {
+          graph[connector,j] = i
+          break
+        }
+        else if (j == length(graph[connector,]))
+        {
+          #Need to add another column
+          graph = cbind(graph,0)
+          graph[connector,(j+1)] = i
+        }
+      }
+      
+      for(j in 5:length(graph[i,]))
+      {
+        if (graph[i,j] == 0)
+        {
+          graph[i,j] = connector
+          break
+        }
+        else if (j == length(graph[i,]))
+        {
+          #Need to add another column
+          graph = cbind(graph,0)
+          graph[i,(j+1)] = connector
+        }
+      }
+    }
+  }
+  graph
+}
+
 #Complete Graph
 complete.graph <- function(popSize)
 {
@@ -40,6 +214,7 @@ complete.graph <- function(popSize)
   return(t(graph))
 }
 
+#Larger grid
 gridConstructor.withDiag <-function(popSize)
 {
   sideLength = sqrt(popSize)
@@ -155,7 +330,6 @@ gridConstructor.withDiag <-function(popSize)
   graph
 }
 
-
 #Creates a grid structure - popSize must be a perfect square
 gridConstructor <- function(popSize)
 {
@@ -206,6 +380,61 @@ gridConstructor <- function(popSize)
   
   graph
 }
+
+randomConstructor <- function(avgConnections, popSize)
+{
+  graph = matrix(0,nrow=popSize,ncol=1)
+
+  for (i in 1:(popSize*avgConnections/2 - 1))
+  {
+    if (i <= popSize)
+    {
+      #Make sure ever node is selected at least once
+      firstNode = i
+    }
+    else
+    {
+      firstNode = sample(1:(popSize-1),1)
+    }
+    
+    secondNode = sample(1:(popSize-1),1)
+    if (secondNode == firstNode) secondNode = popSize #We don't want a self connection
+    
+    for(j in 1:length(graph[secondNode,]))
+    {
+      if (graph[secondNode,j] == 0)
+      {
+        graph[secondNode,j] = firstNode
+        break
+      }
+      else if (j == length(graph[secondNode,]))
+      {
+        #Need to add another column
+        graph = cbind(graph,0)
+        graph[secondNode,(j+1)] = firstNode
+      }
+    }
+    
+    for(j in 1:length(graph[firstNode,]))
+    {
+      if (graph[firstNode,j] == 0)
+      {
+        graph[firstNode,j] = secondNode
+        break
+      }
+      else if (j == length(graph[firstNode,]))
+      {
+        #Need to add another column
+        graph = cbind(graph,0)
+        graph[firstNode,(j+1)] = secondNode
+      }
+    }
+  }
+  graph
+}
+
+
+
 #graph = gridConstructor(100)
 
 #Test the spatial constructor
