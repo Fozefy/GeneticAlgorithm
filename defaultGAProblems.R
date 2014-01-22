daa5b82495d26c937b99b9be34bf5426f81c2d91
Fozefy <fozefy@gmail.com>
2014-01-21 23:41:47 -0500

    Adding new test problems
    Adding one.max with coupling and a one.max predator prey function.

--- defaultGAProblems.R
+++ defaultGAProblems.R
@@ -8,13 +8,67 @@ finite.min.fn <- function(genes, gene.max){
 }
 
 #Variant of one.max with two pops, takes a gene of the same id in the other pop and adds at half value to fitness
-twoPop.one.max <- function(organism, popNum, otherPops, externalConnectionsMatrix){
-  otherGenes = otherPops[[1]]@organisms$values[[externalConnectionsMatrix[organism@index, popNum]]]@chromosome$genes
-  sum(organism@chromosome$genes)*2 + sum(otherGenes)
+twoPop.one.max.withCoupling <- function(coupling)
+{
+  twoPop.one.max <- function(organism, popNum, otherPops, externalConnectionsMatrix){
+    otherGenes = otherPops[[1]]@organisms$values[[externalConnectionsMatrix[organism@index, popNum]]]@chromosome$genes
+    sum(organism@chromosome$genes)*coupling + sum(otherGenes)*(1 - coupling)
+  }
 }
 
 twoPop.one.max.seperate <- function(organism, pop, otherPops, externalConnectionsMatrix){
   sum(organism@chromosome$genes)*3
+}
+
+twoPop.one.max.predPrey <- function(organism, popNum, otherPops, externalConnectionsMatrix)
+{
+  otherGenes = otherPops[[1]]@organisms$values[[externalConnectionsMatrix[organism@index, popNum]]]@chromosome$genes
+  
+  if(popNum == 1)
+  {
+    #We are the primary Population (predator)
+    #Fitness = geneLength - each 0 where prey has 1
+    fitness = length(organism@chromosome$genes) - sum(organism@chromosome$genes == 0 & otherGenes == 1)
+  }
+  else
+  {
+    #We are the secondary Population (prey)
+    #Fitness = for each 1 where predator has 0
+    fitness = sum(organism@chromosome$genes == 1 & otherGenes == 0)    
+  }
+  return(fitness)
+}
+
+twoPop.one.max.predPrey.goal.fn <- function(goal, epsilon = 0, maximizing = TRUE)
+{
+  consecutiveMax = 0
+  goal.fn <-function(popFit)
+  {
+    if (maximizing)
+    {
+      if (max(popFit[[1]]) >= (goal - epsilon)) 
+      {
+        consecutiveMax <<- consecutiveMax + 1
+      }
+      else
+      {
+        consecutiveMax <<- 0
+      }
+    }
+    else
+    {
+      if (min(popFit[[1]]) <= (goal + epsilon))
+      {
+        consecutiveMax <<- consecutiveMax + 1
+      }
+      else
+      {
+        consecutiveMax <<- 0
+      }
+    }
+
+    if (consecutiveMax == 5) return(TRUE) else return(FALSE)
+  }
 }
 
 abc.fit <-function(organism)
